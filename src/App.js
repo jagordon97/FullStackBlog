ea68f9f087cf69cc7d08e3421fb37a23facf28b4
Justin Gordon <justingordon@Justins-MacBook-Pro.local>
2024-09-13 09:44:03 -0600

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
     </div>
   );
+}
+
+function Header(){
+  return (
+    <header className="App-header">
+        <div class="flex--all-center">
+          <div className="App-logo-div flex--all-center">
+            <img src={logo} className="App-logo"  alt="logo" />
+          </div>
+          
+          <div>
+            Squash Collective
+          </div>
+        </div>
+        <Login />
+        
+      </header>
+  )
+}
+
+
+function Login(){
+  return (
+    <a
+      className="Login"
+      href="https://reactjs.org"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Log in
+    </a>
+  )
 }
 
 export default App;
